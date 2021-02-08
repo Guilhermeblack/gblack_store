f806451cc19419289344b86472338418be8c30b3
guilhermeblack <guilhermeglm1994@gmail.com>
2021-02-08 07:32:03 -0300

    'desenv_db'

--- loja/models.py
+++ loja/models.py
@@ -1,367 +1,280 @@
-# class Gerente(models.Model):
-#
-#     class Meta:
-#         permissions = [
-#             ('fazer_pedido', 'incluir pedido'),
-#             ('ver_feed', 'visualizar o feed'),
-#             ('iniciar_movimento', 'iniciar o movimento'),
-#             ('fechar_comanda', 'fechar a comanda'),
-#             ('abrir_comanda', 'abrir uma nova comanda'),
-#             ('controlar_produtos', 'controlar produtos disponiveis')
-#         ]
-#
-#         # app_label = 'gerente'
-#
-#     def __str__(self):
-#         return self.nome
-#
-#     id = models.AutoField(primary_key=True),
-#     nome = models.CharField(
-#         max_length=50,
-#         null=False,
-#         blank=False,
-#         default='gerente'
-#     ),
-#     senha = models.CharField(
-#         max_length=50,
-#         null=False,
-#         blank=False,
-#     ),
-#     senha_rep = models.CharField(
-#         max_length=50,
-#         null=False,
-#         blank=False,
-#
-#     ),
-#
-#     objects = models.Manager()
-#
-# class Garcom(models.Model):
-#     class Meta:
-#         permissions = [
-#             ('fazer_pedido', 'incluir pedido'),
-#             ('pedido_entregue', 'pedido foi entregue'),
-#             ('ver_feed', 'visualizar o feed'),
-#             ('abrir_comanda', 'abrir uma nova comanda')
-#         ]
-#
-#     def __str__(self):
-#         return self.nome
-#
-#     id = models.AutoField(primary_key=True),
-#
-#     nome = models.CharField(
-#         max_length=50,
-#         null=False,
-#         blank=False,
-#         default="garçom",
-#     ),
-#     senha = models.CharField(
-#         max_length=50,
-#         null=False,
-#         blank=False,
-#     ),
-#     senha_rep = models.CharField(
-#         max_length=50,
-#         null=False,
-#         blank=False,
-#     ),
-#     objects = models.Manager()
-#
-# class Cozinha(models.Model):
-#     class Meta:
-#         permissions = [
-#             ('pedido_pronto', 'pedido pronto'),
-#             ('ver_feed', 'visualizar o feed')
-#         ]
-#
-#     def __str__(self):
-#         return self.nome
-#
-#     id = models.AutoField(primary_key=True),
-#
-#     nome = models.CharField(
-#         max_length=50,
-#         null=False,
-#         blank=False,
-#         default='cheff',
-#     )
-#     senha = models.CharField(
-#         max_length=50,
-#         null=False,
-#         blank=False,
-#     )
-#     senha_rep = models.CharField(
-#         max_length=50,
-#         null=False,
-#         blank=False,
-#     )
-#     objects = models.Manager()
-#
-#
-# class Caixa(models.Model):
-#     class Meta:
-#         permissions = [
-#             ('fechar_comanda', 'fechar a comanda'),
-#             ('ver_feed', 'visualizar o feed')
-#         ]
-#
-#     def __str__(self):
-#         return self.nome
-#
-#     id = models.AutoField(primary_key=True),
-#
-#
-#     nome = models.CharField(
-#         max_length=50,
-#         null=False,
-#         blank=False,
-#         default='cheff',
-#     )
-#     senha = models.CharField(
-#         max_length=50,
-#         null=False,
-#         blank=False,
-#     )
-#     senha_rep = models.CharField(
-#         max_length=50,
-#         null=False,
-#         blank=False,
-#     )
-#     objects = models.Manager()
-#
-#
-# class Comanda(models.Model):
-#
-#     id = models.AutoField(primary_key=True)
-#
-#
-#     nome = models.CharField(
-#         max_length=50,
-#         null=False,
-#         blank=False,
-#
-#     )
-#
-#     n_mesa = models.IntegerField(
-#         null=False,
-#         blank=False,
-#     )
-#
-#     valor = models.FloatField(
-#         default=0.0,
-#         null=False,
-#         blank=True
-#
-#     )
-#
-#     data = models.DateTimeField(auto_now_add=True, blank=True)
-#
-#     STATUS_CHOICES = (
-#         ("A", "Aberto"),
-#         ("F", "Fechado"),
-#     )
-#     status = models.CharField(
-#         max_length=1,
-#         choices=STATUS_CHOICES,
-#         default="A",
-#         blank=True,
-#         null=False
-#     )
-#
-#
-#     def __str__(self):
-#         return ' {}'.format(self.nome)
-#
-#     objects = models.Manager()
-#
-#
-# class Produtocad(models.Model):
-#
-#     id = models.AutoField(primary_key=True)
-#
-#
-#     nome = models.CharField(
-#         max_length=50,
-#         null=False,
-#         blank=False
-#     )
-#     descricao = models.TextField(
-#         max_length=255,
-#         null=False,
-#         blank=False
-#     )
-#     preco = models.FloatField(
-#         null=False,
-#         blank=False,
-#         default=0.0
-#     )
-#
-#     # pedidoProdutos = models.ManyToManyField(Pedido)
-#
-#     STATUS_CHOICES = (
-#         ("A", "Alimento"),
-#         ("B", "Bebida"),
-#     )
-#     tipo = models.CharField(
-#         max_length=1,
-#         choices=STATUS_CHOICES,
-#         blank=False,
-#         null=False
-#     )
-#
-#     img_prod = CloudinaryField()
-#
-#     def __str__(self):
-#         return self.nome
-#
-#     objects = models.Manager()
-#
-# class Pedido(models.Model):
-#
-#     STATUS_CHOICES = (
-#         ("P", "Pedido realizado"),
-#         ("F", "Fazendo"),
-#         ("S", "Saiu para entrega"),
-#         ("E", "Foi entregue"),
-#         ("C", "Cancelado")
-#     )
-#
-#     STATUS_PGT = (
-#         ("P", "Pago"),
-#         ("R", "Resto"),
-#         ("N", "Não pago")
-#     )
-#
-#
-#     comandaref= models.ForeignKey(
-#         Comanda,
-#         null=True,
-#         on_delete=models.CASCADE,
-#         related_name="pedidoComanda"
-#     )
-#
-#     id = models.AutoField(primary_key=True)
-#
-#
-#     produtosPed= models.ManyToManyField(Produtocad, related_name='produto', blank=False)
-#
-#     quantidade = models.IntegerField(
-#         null=False,
-#         blank=False,
-#         default= 1
-#     )
-#     observacao = models.TextField(
-#         max_length=170,
-#         null=False,
-#         blank=True,
-#         default='Sem Observações.',
-#
-#     )
-#
-#     status = models.CharField(
-#         max_length=1,
-#         choices=STATUS_CHOICES,
-#         blank=False,
-#         null=False,
-#         default="P"
-#     )
-#
-#     status_pago = models.CharField(
-#         max_length=1,
-#         choices=STATUS_PGT,
-#         blank=False,
-#         null=False,
-#         default="N"
-#
-#     )
-#
-#     valor = models.FloatField(
-#         default=0.0,
-#         null=False,
-#         blank=True
-#
-#     )
-#
-#     data = models.DateTimeField(auto_now_add=True , blank=True)
-#
-#     def __str__(self):
-#         return '{} - {}'.format( self.id, self.comandaref)
-#
-#     objects = models.Manager()
-#
-# class pagamentos(models.Model):
-#
-#     id = models.AutoField(primary_key=True)
-#
-#
-#     pedidored = models.ManyToManyField(Pedido, related_name='pedidored', blank=True)
-#
-#     STATUS_PGT = (
-#         ("F", "fechou comanda"),
-#         ("P", "pagou produto"),
-#         ("R", "Restante")
-#     )
-#
-#     FORMA_PGT = (
-#         ("C", "Cartão"),
-#         ("P", "Pix"),
-#         ("D", "Dinheiro")
-#     )
-#
-#     formapg = models.CharField(
-#         max_length=1,
-#         choices=FORMA_PGT,
-#         blank=False,
-#         null=False,
-#         default="D"
-#     )
-#
-#     status = models.CharField(
-#         max_length=1,
-#         choices=STATUS_PGT,
-#         blank=False,
-#         null=False,
-#         default="R"
-#     )
-#
-#
-#     valor = models.FloatField(
-#         default=0.0,
-#         null=False,
-#         blank=True
-#     )
-#
-#     data = models.DateTimeField(auto_now_add=True, blank=True)
-#
-#     def __str__(self):
-#         return '{}'.format(self.id)
-#
-#     objects = models.Manager()
-#
-# class logform(models.Model):
-#     senha = models.CharField(
-#         max_length=80,
-#         null=False,
-#         blank=False,
-#     )
-#     nome = models.CharField(
-#         max_length=80,
-#         null=False,
-#         blank=False,
-#     )
-#
-# class movi(models.Model):
-#     STATUS_CHOICES = (
-#         ("L", "Ligado"),
-#         ("D", "Desligado"),
-#     )
-#
-#     movimento = models.CharField(
-#         max_length=1,
-#         choices=STATUS_CHOICES,
-#         blank=False,
-#         null=False
-#     )
-#     objects = models.Manager()
-# # Create your models here.
+from django.db import models
+from cloudinary.models import CloudinaryField
+from django.utils.timezone import now
+
+class Cliente(models.Model):
+
+    class Meta:
+        permissions = [
+            ('adicionar_prod', 'Adicionar produto'),
+            ('pagar_prod', 'Pagar produtos'),
+
+        ]
+
+        # app_label = 'Cliente'
+
+
+    id = models.AutoField(primary_key=True),
+
+    nome = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+        default='gerente'
+    ),
+    senha = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+    ),
+    telefone = models.CharField(
+        max_length=20,
+        null=False,
+        blank=False,
+
+    ),
+    email = models.CharField(
+        max_length=35,
+        null=False,
+        blank=False,
+
+    ),
+    cpf = models.CharField(
+        max_length=16,
+        null=False,
+        blank=False,
+
+    ),
+
+    objects = models.Manager()
+
+class Produto(models.Model):
+
+    id = models.AutoField(primary_key=True)
+
+
+    nome = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False
+    )
+    descricao = models.TextField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+    preco = models.FloatField(
+        null=False,
+        blank=False,
+        default=0.0
+    )
+
+    img_prod = CloudinaryField()
+
+    STATUS_CHOICES = (
+        ("R", "Relogio"),
+        ("A", "Acessorio"),
+        ("v", "Vesturaio"),
+    )
+    tipo = models.CharField(
+        max_length=1,
+        choices=STATUS_CHOICES,
+        blank=False,
+        null=False
+    )
+
+    # def __str__(self):
+    #     return self.nome
+
+    objects = models.Manager()
+
+class Carrinho(models.Model):
+
+    cliente_cli = models.ForeignKey(
+        Cliente,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="Cliente"
+    )
+
+    id = models.AutoField(primary_key=True),
+
+
+
+    produto_cli= models.ManyToManyField(Produto, related_name='produto', blank=False)
+
+    valor = models.FloatField(
+        default=0.0,
+        null=False,
+        blank=True
+
+    )
+
+    objects = models.Manager()
+
+
+class Tot_ped(models.Model):
+
+
+    # def __str__(self):
+    #     return self.id
+
+    id = models.AutoField(primary_key=True),
+    carrinho = models.ForeignKey(
+        Carrinho,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="carrinho"
+    )
+
+    quantidade = models.IntegerField(
+        null=False,
+        blank=False,
+        default=1
+    )
+
+    produto_carrinho = models.ForeignKey(
+        Produto,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="prod_carrinho"
+    )
+
+    objects = models.Manager()
+
+
+class Venda(models.Model):
+
+    STATUS_CHOICES = (
+        ("P", "Pedido realizado"),
+        ("F", "Fazendo"),
+        ("S", "Saiu para entrega"),
+        ("E", "Foi entregue"),
+        ("C", "Cancelado")
+    )
+
+    STATUS_PGT = (
+        ("P", "Pago"),
+        ("R", "Resto"),
+        ("N", "Não pago")
+    )
+
+    carrinho = models.ForeignKey(
+        Carrinho,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="carrinho_cli"
+    )
+
+    id = models.AutoField(primary_key=True)
+
+
+    produto_cli= models.ManyToManyField(Produto, related_name='produto_cli', blank=False)
+
+    quantidade = models.IntegerField(
+        null=False,
+        blank=False,
+        default= 1
+    )
+    observacao = models.TextField(
+        max_length=170,
+        null=False,
+        blank=True,
+        default='Sem Observações.',
+
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS_CHOICES,
+        blank=False,
+        null=False,
+        default="P"
+    )
+
+    status_pago = models.CharField(
+        max_length=1,
+        choices=STATUS_PGT,
+        blank=False,
+        null=False,
+        default="N"
+
+    )
+
+    valor = models.FloatField(
+        default=0.0,
+        null=False,
+        blank=True
+
+    )
+
+    data = models.DateTimeField(auto_now_add=True , blank=True)
+
+    # def __str__(self):
+    #     return '{} - {}'.format( self.id, self.carrinho)
+
+    objects = models.Manager()
+
+class Pagamentos(models.Model):
+
+    id = models.AutoField(primary_key=True)
+
+
+    carrinho_ped = models.ManyToManyField(Carrinho, related_name='carribgo_ped', blank=True)
+
+    STATUS_PGT = (
+        ("C", "Completo"),
+        ("P", "Processo"),
+        ("R", "Recusado")
+    )
+
+    FORMA_PGT = (
+        ("C", "Cartão"),
+        ("P", "Pix"),
+        ("D", "Dinheiro")
+    )
+
+    formapg = models.CharField(
+        max_length=1,
+        choices=FORMA_PGT,
+        blank=False,
+        null=False,
+        default="D"
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS_PGT,
+        blank=False,
+        null=False,
+        default="R"
+    )
+
+
+    valor = models.FloatField(
+        default=0.0,
+        null=False,
+        blank=True
+    )
+
+    data = models.DateTimeField(auto_now_add=True, blank=True)
+
+    # def __str__(self):
+    #     return '{}'.format(self.id)
+
+    objects = models.Manager()
+
+
+
+class logform(models.Model):
+    senha = models.CharField(
+        max_length=80,
+        null=False,
+        blank=False,
+    )
+    nome = models.CharField(
+        max_length=80,
+        null=False,
+        blank=False,
+    )
+
