398866ecd6c27b10135ee90d847389b1f3171c2e
guilhermeblack <guilhermeglm1994@gmail.com>
2021-02-14 10:09:17 -0300

    'vamola'

--- gbstr/urls.py
+++ gbstr/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from django.contrib import admin
 from loja import views
+
 from django.conf import urls, settings
+from gbstr import settings
 from django.conf.urls.static import static
 
 app_name = 'gbstr'
